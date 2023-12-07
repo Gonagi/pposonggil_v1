c3dbda8628422c4ed7a9d287b1e31d0f82a7c0ce
Gonagi <kkh10002@naver.com>
2023-12-07 13:15:07 +0900

    2023.12.07 김건학 입력받은 시각('HH:MM')의 base_time('HH:00')을 리턴하는 함수 convertTimeFromat 구현 입력받은 시각('HH:MM')에 int형 time을 더한 결과('HH:MM')를 리턴하는 함수 addTime 구현

--- public/js/cal_time.js
+++ public/js/cal_time.js
@@ -46,4 +46,35 @@ function leadingZeros(n, digits) {
   return n.toString().padStart(digits, "0");
 }
 
+// 2023.12.07 김건학
+// 입력받은 시각('HH:MM')의 base_time('HH:00')을 리턴하는 함수
+export function convertTimeFormat(time) {
+  var basetime = time.replace(":", "");
+  basetime = basetime.replace(/(\d{2})(\d{2})/, "$100");
+  return basetime;
+}
+
+// 입력받은 시각('HH:MM')에 int형 time을 더한 결과('HH:MM')를 리턴하는 함수
+export function addTime(cur_time, time) {
+  // ":"를 기준으로 시간과 분을 분리
+  var [cur_hours, cur_minutes] = cur_time.split(":").map(Number);
+
+  cur_minutes += time;
+
+  if (cur_minutes >= 60) {
+    cur_minutes -= 60;
+    cur_hours += 1;
+  }
+  if (cur_hours >= 24) {
+    cur_hours -= 24;
+  }
+
+  // 문자열로 변환하고, 한자리 수일 경우 '0'을 추가
+  const result_hours = cur_hours.toString().padStart(2, "0");
+  const result_minutes = cur_minutes.toString().padStart(2, "0");
+
+  // ":"로 구분된 문자열을 리턴
+  return `${result_hours}:${result_minutes}`;
+}
+
 window.getTimeStamp = getTimeStamp; // 전역 스코프에 getTimeStamp 함수 추가
